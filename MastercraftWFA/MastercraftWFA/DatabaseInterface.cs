9714124dbaa0499500c0cfb089ff3a124c08af67
Theo Harkes <taharkes@gmail.com>
2017-05-26 23:42:48 +0200

    Moved: some code to databaseinterface

--- MastercraftWFA/MastercraftWFA/DatabaseInterface.cs
+++ MastercraftWFA/MastercraftWFA/DatabaseInterface.cs
@@ -1,26 +1,52 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Windows.Forms;
 
 namespace MastercraftWFA {
-    static class DatabaseInterface {
+    class DatabaseInterface {
 
-        static public DataTable GetProfessionsTable() {
+        public DataTable GetProfessionsTable() {
+            return Database.GetProfessions();
+        }
+
+        public DataTable GetRecipesTable(string profession) {
+            DataTable costTable = Database.GetRecipesCost(profession);
+            DataTable tier1Table = Database.GetRecipesReward(profession, 1);
+            DataTable tier2Table = Database.GetRecipesReward(profession, 2);
+            DataTable tier3Table = Database.GetRecipesReward(profession, 3);
+            costTable.Merge(tier1Table);
+            costTable.Merge(tier2Table);
+            costTable.Merge(tier3Table);
+            costTable.Columns.Add("tier1profit", typeof(int), "tier1reward - cost");
+            costTable.Columns.Add("tier2profit", typeof(int), "tier2reward - cost");
+            costTable.Columns.Add("tier3profit", typeof(int), "tier3reward - cost");
+            costTable.Columns.Add("profit", typeof(int));
+            // Compute profit value
+            for (int i = 0; i < costTable.Rows.Count; i++) {
+                bool tool = Database.HasTool(profession);
+                Int64 cost = costTable.Rows[i].Field<Int64>("cost");
+                Int64 tier1Reward = costTable.Rows[i].Field<Int64>("tier1reward");
+                Int64 tier2Reward = costTable.Rows[i].Field<Int64>("tier2reward");
+                Int64 tier3Reward = costTable.Rows[i].Field<Int64>("tier3reward");
+                Int64 tier3ChanceReward = (int)(tool ? 0.75 * tier3Reward + 0.25 * tier2Reward : 0.35 * tier3Reward + 0.65 * tier2Reward);
+                Int64 profit = Math.Max(tier1Reward, tier2Reward);
+                profit = Math.Max(profit, tier3ChanceReward);
+                profit -= cost;
+                costTable.Rows[i].SetField("profit", profit);
+            }
+            return costTable;
+        }
+
+        public DataTable GetResourcesTable(List<string> recipes) {
             return new DataTable();
         }
 
-        static public DataTable GetRecipesTable(List<string> professions) {
+        public DataTable GetResultsTable(List<string> recipes) {
             return new DataTable();
         }
 
-        static public DataTable GetResourcesTable(List<string> recipes) {
-            return new DataTable();
-        }
-
-        static public DataTable GetResultsTable(List<string> recipes) {
-            return new DataTable();
-        }
-
-        static public DataTable GetConsumedResourcesTable(List<string> recipes) {
+        public DataTable GetConsumedResourcesTable(List<string> recipes) {
             return new DataTable();
         }
     }
--- MastercraftWFA/MastercraftWFA/Main.cs
+++ MastercraftWFA/MastercraftWFA/Main.cs
@@ -5,11 +5,12 @@ using System.Windows.Forms;
 
 namespace MastercraftWFA {
     public partial class Main : Form {
-
+        DatabaseInterface database;
         string activeProfession;
 
         public Main() {
             InitializeComponent();
+            database = new DatabaseInterface();
             FillDataProfessions();
             FillDataResources();
         }
@@ -17,10 +18,9 @@ namespace MastercraftWFA {
 
         #region Professions Methods
         void FillDataProfessions() {
-            List<int> gradeOptions = new List<int>() { 0, 1, 2, 3 };
             DataGridViewComboBoxColumn toolComboBoxColumn = (DataGridViewComboBoxColumn)dataGridViewProfessions.Columns["grade"];
-            toolComboBoxColumn.DataSource = gradeOptions;
-            dataGridViewProfessions.DataSource = Database.GetProfessions();
+            toolComboBoxColumn.DataSource = new List<int>() { 0, 1, 2, 3 };
+            dataGridViewProfessions.DataSource = database.GetProfessionsTable();
         }
 
         private void DataGridViewProfessions_CellClick(object sender, DataGridViewCellEventArgs e) {
@@ -58,31 +58,7 @@ namespace MastercraftWFA {
 
         #region Recipes Methods
         void FillDataRecipes(string profession) {
-            DataTable costTable = Database.GetRecipesCost(profession);
-            DataTable tier1Table = Database.GetRecipesReward(profession, 1);
-            DataTable tier2Table = Database.GetRecipesReward(profession, 2);
-            DataTable tier3Table = Database.GetRecipesReward(profession, 3);
-            costTable.Merge(tier1Table);
-            costTable.Merge(tier2Table);
-            costTable.Merge(tier3Table);
-            costTable.Columns.Add("tier1profit", typeof(int), "tier1reward - cost");
-            costTable.Columns.Add("tier2profit", typeof(int), "tier2reward - cost");
-            costTable.Columns.Add("tier3profit", typeof(int), "tier3reward - cost");
-            costTable.Columns.Add("profit", typeof(int));
-            // Compute profit value
-            for (int i = 0; i < costTable.Rows.Count; i++) {
-                bool tool = Database.HasTool(profession);
-                Int64 cost = costTable.Rows[i].Field<Int64>("cost");
-                Int64 tier1Reward = costTable.Rows[i].Field<Int64>("tier1reward");
-                Int64 tier2Reward = costTable.Rows[i].Field<Int64>("tier2reward");
-                Int64 tier3Reward = costTable.Rows[i].Field<Int64>("tier3reward");
-                Int64 tier3ChanceReward = (int)(tool ? 0.75 * tier3Reward + 0.25 * tier2Reward : 0.35 * tier3Reward + 0.65 * tier2Reward);
-                Int64 profit = Math.Max(tier1Reward, tier2Reward);
-                profit = Math.Max(profit, tier3ChanceReward);
-                profit -= cost;
-                costTable.Rows[i].SetField("profit", profit);
-            }
-            dataGridViewRecipes.DataSource = costTable;
+            dataGridViewRecipes.DataSource = database.GetRecipesTable(profession);
         }
 
         private void DataGridViewRecipes_CellClick(object sender, DataGridViewCellEventArgs e) {
