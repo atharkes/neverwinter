07c066bf89e7d07c6c623b4824b3ac01f1be5c85
Theo Harkes (Desktop) <taharkes@gmail.com>
2018-05-05 20:05:33 +0200

    Add: Class structure as new project

---
+++ Mastercrafting/Mastercrafter/Profession.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Mastercrafter {
+    /// <summary> A profession has recipes and certain grade </summary>
+    public class Profession {
+        /// <summary> The name of this profession </summary>
+        public string Name { get; }
+        /// <summary> The grade of this profession </summary>
+        public int Grade { get; }
+        /// <summary> The recipes in this profession </summary>
+        public List<Recipe> Recipes { get; }
+
+        /// <summary> The resources that are needed to upgrade this profession </summary>
+        public List<(Resource Resource, int Amount)>[] Upgrade { get; }
+        /// <summary> The amount of profession grades currently in the game </summary>
+        public const int Grades = 4;
+
+        /// <summary> Create a new profession object with no recipes </summary>
+        public Profession(string name) {
+            Name = name;
+            Grade = 0;
+            Recipes = new List<Recipe>();
+            Upgrade = new List<(Resource, int)>[Grades];
+            for (int i = 0; i < Grades; i++) {
+                Upgrade[i] = new List<(Resource, int)>();
+            }
+        }
+        
+        /// <summary> The recipes that are currently available corresponding to the grade </summary>
+        /// <returns>The recipes that are available</returns>
+        public List<Recipe> AvailableRecipes() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Mastercrafting/Mastercrafter/Recipe.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace Mastercrafter {
+    /// <summary> A recipe that consumes resources to create results </summary>
+    public class Recipe {
+        /// <summary> The name of this recipe </summary>
+        public string Name { get; }
+        /// <summary> The grade of this recipe </summary>
+        public int Grade { get; }
+        /// <summary> The cost in guildmarks to instantly get a tier 3 result. 0 indicates it can't be done </summary>
+        public int GuildMarks { get; }
+        /// <summary> The cost to instantly complete this recipe </summary>
+        public int AstralDiamonds { get; }
+
+        /// <summary> The resources and the amount that are consumed by this recipe </summary>
+        public List<(Resource Resource, int Amount)> Consumed { get; }
+        /// <summary> The results per tier of this recipe </summary>
+        public List<(Resource Resource, int Amount)>[] Result { get; }
+        /// <summary> The amount of tiers available </summary>
+        public const int Tiers = 3;
+
+        /// <summary> Create a new recipe object with empty lists </summary>
+        public Recipe(string name, int guildMarks = 0) {
+            Name = name;
+            GuildMarks = guildMarks;
+            AstralDiamonds = 20_000;
+            Consumed = new List<(Resource, int)>();
+            Result = new List<(Resource, int)>[Tiers];
+            for (int i = 0; i < Tiers; i++) {
+                Result[i] = new List<(Resource, int)>();
+            }
+        }
+
+        /// <summary> The cost to start this recipe in astral diamonds </summary>
+        /// <returns>The amount of astral diamonds the resources cost</returns>
+        public int ConsumedCost() {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary> The expected profit of this recipe </summary>
+        /// <param name="tier">The tier percentage the recipe is done at</param>
+        /// <returns>The profit in astral diamonds</returns>
+        public int Profit(float tier = 1.75f) {
+            throw new System.NotImplementedException();
+        }
+    }
+}
