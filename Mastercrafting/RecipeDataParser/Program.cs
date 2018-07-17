b039c630fa1b983f63f4693bf214fc4064d1dcba
Theo Harkes <taharkes@gmail.com>
2018-07-17 12:42:04 +0200

    Added: identification of row type

--- Mastercrafting/RecipeDataParser/Program.cs
+++ Mastercrafting/RecipeDataParser/Program.cs
@@ -10,7 +10,7 @@ namespace RecipeDataParser {
         public static string ProfessionDataFolder => Path.Combine(Environment.CurrentDirectory, @"RecipeData");
 
         /// <summary> The types of rows in the data </summary>
-        public enum RowTypes {
+        public enum RowType {
             Empty,
             TierDelimiter,
             TierHeader,
@@ -33,11 +33,36 @@ namespace RecipeDataParser {
             foreach (string filePath in Directory.EnumerateFiles(ProfessionDataFolder)) {
                 Console.WriteLine("Reading File: {0}", Path.GetFileName(filePath));
                 using (StreamReader sr = new StreamReader(filePath)) {
-                   
+                    while (!sr.EndOfStream) {
+                        string[] row = sr.ReadLine().Split(';');
+                        Console.WriteLine(IdentifyRow(row));
+                    }
                 }
             }
 
             Database.CloseDatabase();
+        }
+
+        /// <summary> Identify the type a row </summary>
+        /// <param name="row">The row to identify the type for</param>
+        /// <returns>The type of the row</returns>
+        static RowType IdentifyRow(string[] row) {
+            if (row[0].StartsWith("Masterwork")) {
+                return RowType.TierDelimiter;
+            }
+            if (row[0].StartsWith("Level Req.")) {
+                return RowType.TierHeader;
+            }
+            if (int.TryParse(row[0], out int levelRequired)) {
+                return RowType.RecipeInitial;
+            }
+            if (row[3].StartsWith("Consumes")) {
+                return RowType.RecipeConsumes;
+            }
+            if (row[7].StartsWith("Tier 3")) {
+                return RowType.RecipeFinal;
+            }
+            return RowType.Empty;
         }
     }
 }
