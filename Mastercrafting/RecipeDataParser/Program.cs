69bf32f0cdcff21b4d686f34445b6b0b59973464
Theo Harkes <taharkes@gmail.com>
2018-07-17 12:19:55 +0200

    Moved: RecipeData to the RecipeDataParser; Added: RowTypes to RecipeDataParser

--- Mastercrafting/RecipeDataParser/Program.cs
+++ Mastercrafting/RecipeDataParser/Program.cs
@@ -1,20 +1,43 @@
 ﻿using DatabaseInterface;
 using DatabaseInterface.Data;
+using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace RecipeDataParser {
     class Program {
+        /// <summary> The folder in which the data files of the different professions are stored </summary>
+        public static string ProfessionDataFolder => Path.Combine(Environment.CurrentDirectory, @"RecipeData");
+
+        /// <summary> The types of rows in the data </summary>
+        public enum RowTypes {
+            Empty,
+            TierDelimiter,
+            TierHeader,
+            RecipeInitial,
+            RecipeConsumes,
+            RecipeFinal
+        }
+
+        /// <summary> Main call point of the RecipeDataParser project </summary>
+        /// <param name="args">The arguments given on execution</param>
         static void Main(string[] args) {
             Database.InitializeDatabase("test.sqlite");
 
-            Profession.Factory.CreateProfession("Artificing", 3);
-            Profession.Factory.CreateProfession("Jewelcrafting", 3);
-
+            // Load all data currently in the database
             List<Profession> professions = Profession.Factory.LoadProfessions();
             List<Resource> resources = Resource.Factory.LoadResources();
             List<Recipe> recipes = Recipe.Factory.LoadRecipes();
             Recipe.Factory.LoadRecipeCosts();
             Recipe.Factory.LoadRecipeResults();
+
+            // Parse profession data
+            foreach (string filePath in Directory.EnumerateFiles(ProfessionDataFolder)) {
+                Console.WriteLine("Reading File: {0}", Path.GetFileName(filePath));
+                using (StreamReader sr = new StreamReader(filePath)) {
+                   
+                }
+            }
 
             Database.CloseDatabase();
         }
